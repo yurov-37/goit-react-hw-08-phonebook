2f53e49321e05650fababdd2d1c2d6b3264b88d2
yurov-37 <104888831+yurov-37@users.noreply.github.com>
2023-01-13 15:07:40 +0000

    Deploying to gh-pages from @ yurov-37/goit-react-hw-08-phonebook@789ba126e67034ff09c61cc4288871347fba6d11 🚀

---
+++ static/js/795.01fbae37.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_04_phonebook=self.webpackChunkgoit_react_hw_04_phonebook||[]).push([[795],{1795:function(n,e,t){t.r(e),t.d(e,{default:function(){return r}});var o=t(890),c=t(8870),p=t(184);function r(){return(0,p.jsxs)(o.Z,{variant:"h2",component:"h1",padding:"50px 24px 0",textAlign:"center",children:["ContactsApp welcome page ",(0,p.jsx)(c.Z,{component:"span",children:"\ud83d\udcf1"})]})}}}]);
+//# sourceMappingURL=795.01fbae37.chunk.js.map
